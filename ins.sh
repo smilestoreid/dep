45928a3bfab62e4327f2b303ae473b705ca95283
root <root@localhost.localdomain>
2022-10-29 06:17:43 +0200

    Up

--- ins.sh
+++ ins.sh
@@ -3,3 +3,5 @@ cp rsa_host_key /etc/smile/rsa_host_key
 cp ecdsa_host_key /etc/smile/ecdsa_host_key
 cp ed25519_host_key /etc/smile/ed25519_host_key
 echo "Done. Copying key"
+mv dropbear /etc/smile
+chmod +x /etc/dropbear
